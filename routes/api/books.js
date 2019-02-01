5cc07c920ca2aaeca5778a178e6c5d6585db665e
Jorge Baez <jibm_82@yahoo.com.mx>
2019-02-01 02:08:37 -0600

    Implemented google books search on server

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,5 +1,6 @@
-const router = require("express").Router();
+const axios = require("axios");
 const booksController = require("../../controllers/booksController");
+const router = require("express").Router();
 
 router
   .route("/")
@@ -11,5 +12,21 @@ router
   .get(booksController.findById)
   .put(booksController.update)
   .delete(booksController.remove);
+
+router.route("/search/:query").get((req, res) => {
+  const apiKey = process.env.GOOGLE_API_KEY;
+  const baseUrl = "https://www.googleapis.com/books/v1/volumes";
+  const url = `${baseUrl}?q=${req.params.query}&key=${apiKey}&langRestrict=en`;
+
+  axios
+    .get(url)
+    .then(response => {
+      res.json(response.data.items);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(422).json(err);
+    });
+});
 
 module.exports = router;
